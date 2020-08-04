94d957498ea9ef684c904cb18ed170ad77b59a98
Irina <irinagmarques@hotmail.com>
2020-08-04 15:21:34 +0200

    solution

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+// console.log("This is my first js file");
+
+/*
+Remove a parenthesis from the first statement and run the code. Does your statement print?
+No
+Is there an error? If so, what kind of error is it?
+SyntaxError: missing ) after argument list
+*/
+
+console.log("Hello World";
