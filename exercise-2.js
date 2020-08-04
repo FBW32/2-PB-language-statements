364cecf962a3ce2bc4032289db306ab25bce53d6
frausafiye <s_gulenc@outlook.com>
2020-08-04 15:20:58 +0200

    added solutions

---
+++ exercise-2.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+// console.log("This is my first js file");
+/*
+1)
+No, it doesnt print the first statement while missing a parenthesis. 
+2)
+I have got this error:
+SyntaxError: missing ) after argument list
+*/
