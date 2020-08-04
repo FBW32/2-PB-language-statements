ace631a0f05c3ddd939efd8c944528f1f7e50176
bartxs <b.soltysik90@gmail.com>
2020-08-04 15:06:02 +0200

    Exercise 2

---
+++ Bartosz/javascript.js
@@ -0,0 +1,9 @@
+// STATEMENTS
+
+console.log("Hello World");
+// console.log"This is my first js file";
+
+// QUESTIONS
+// 1. Statment does not print.
+
+// 2. Error name is: "SyntaxError: Unexpected string"
