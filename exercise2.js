9771d4528d316586cedd06215a96e47ce53ab644
shanti-s <surapanenishanti9@gmail.com>
2020-08-04 14:17:27 +0200

    Done

---
+++ exercise2.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+//console.log("This is my first js file");
+/*
+    1. If i remove parenthesis from the first statement, my statement prints as console.log "Hello World";
+                                                                                             ^^^^^^^^^^^^^ 
+    2. I am getting SyntaxError: Unexpected string                                                                                               
+*/
