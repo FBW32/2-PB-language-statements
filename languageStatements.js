a3dd908a7492903c6b710db24002f0cd89a9d5d7
ben <beatuscanis@hotmail.com>
2020-08-04 14:49:42 +0200

    2-PB Exercise completed

---
+++ languageStatements.js
@@ -0,0 +1,30 @@
+//
+// EXERCISE 1.
+
+// languageStatements.js file create
+
+//
+// EXERCISE 2.
+
+// console.log ("Hello World");
+
+//
+// EXERCISE 3./ 4.
+
+// console.log("This is my first js file.");
+
+//
+//
+// QUESTIONS
+
+//
+// QUESTION 1.
+
+console.log "Hello World";
+
+// The statement does not print
+
+//
+// QUESTION 2.
+
+// The is an error for "SyntaxError: Unexpected string"
